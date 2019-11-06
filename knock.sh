3b187b393b39351aeae8a85d4af677731448293e
Luciano Domingues <luciano.domingues@gmail.com>
2019-11-05 21:36:57 -0300

    add funcoes

--- knock.sh
+++ knock.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 #
-#
 # script knock.sh
 # Script para encontrar malware
-# LAB do treinamento DESEC
-
-#1. Faz a busca nos hosts;
-#2. Interrompe a execucao qdo encontra o malware;
-#3. exibe a pagina HTML do malware;
-#. finaliza o script.
+# LAB DESEC
 
 # Portas (tcp) para ativacao 13,37,30000,3000
 # Malware fica ativo na porta 1337/tcp
+#
 # Autor : Luciano Domingues
 # Data : Out.2019
-# Versao: 1.1
+# Versao: 1.2
 
 ## DEFINICAO DE VARIAVEIS
 rede="172.16.1"
@@ -22,9 +17,33 @@ portas=(13 37 30000 3000)
 ativado="1337"
 conta=1
 
-## INICIA O TESTE NA REDE
-clear
-echo "*** Iniciando scan...."
+## INICIO FUNCOES
+function _limpaTela(){
+	clear
+}
+
+function _mensagemIniciandoScript(){
+	echo "[*] Iniciando scan...."	
+}
+
+function _exibeIpMalware(){
+		echo " ==========================================================="
+		echo "[MALWARE ATIVADO] no IP $rede.$ip ( socket $rede.$ip:$ativado )"
+		echo " ==========================================================="
+		echo " "
+}
+
+function _exibePaginaMalware(){
+		echo " **** Exibindo pagina HTML do Malware =========="
+		wget $rede.$ip:$ativado -O - 2>/dev/null
+		echo " ==========================================================="
+}
+## FIM FUNCOES
+
+## INICIA SCRIPT
+_limpaTela
+_mensagemIniciandoScript
+
 for ip in $(seq 1 254)
 do
 	echo "[$conta] Testando host $rede.$ip "
@@ -32,19 +51,13 @@ do
 	do
 		hping3 -S $rede.$ip -c 1 -p ${portas[$porta]} &>/dev/null
 	done
-
 	# RODA TESTE NA PORTA PARA IDENTIFICAR SE FOI ATIVADA
 		hping3 -S $rede.$ip -c 1 -p $ativado 2>/dev/null | grep "SA" &>/dev/null
 	# CONFIRMA SE A PORTA FOI ABERTA
 	if [ $? -eq 0 ]
 	then
-		echo " ==========================================================="
-		echo "[MALWARE ATIVADO] no IP $rede.$ip ( socket $rede.$ip:$ativado )"
-		echo " ==========================================================="
-		echo " "
-		echo " **** Exibindo pagina HTML do Malware =========="
-		wget $rede.$ip:$ativado -O - 2>/dev/null
-		echo " ==========================================================="
+		_exibeIpMalware
+		_exibePaginaMalware
 		exit
 	fi
 	((conta++))
